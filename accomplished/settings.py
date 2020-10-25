f969207351baa914574f912ed62d76efa1bdf8b0
Michelly Richardson <michellyrichardson@outlook.com>
2020-10-24 22:55:52 -0400

    Updated requirements file

--- accomplished/settings.py
+++ accomplished/settings.py
@@ -115,10 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -128,3 +127,4 @@ LOGIN_REDIRECT_URL = "todolist"
 LOGIN_URL = "login"
 
 django_heroku.setting(locals())
+
