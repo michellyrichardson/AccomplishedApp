44fc4402dced6e37a064030f510ac05f5fa86b60
Michelly Richardson <michellyrichardson@outlook.com>
2020-10-24 22:44:15 -0400

    Final

--- accomplished/settings.py
+++ accomplished/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,10 +119,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "todolist"
 
 LOGIN_URL = "login"
+
+django_heroku.setting(locals())
